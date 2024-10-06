4f4ae7f345840cb415fb757dade19145ddce9fb3
Huste6 <quannghihai1@gmail.com>
2024-10-06 22:40:46 +0700

    học cách sử dụng pipe class-validator

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -12,6 +13,7 @@ async function bootstrap() {
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('ejs');
 
+  app.useGlobalPipes(new ValidationPipe());
   const port = configService.get<string>('PORT'); 
   await app.listen(port);
 }
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,7 +1,20 @@
+import { IsEmail, IsNotEmpty } from "class-validator";
+
 //data transfer object
 export class CreateUserDto {
+    @IsEmail({},{
+        message: "Email khong dung dinh dang"
+    })
+    @IsNotEmpty({
+        message: "Email không được để trống"
+    })
     email: string;
+
+    @IsNotEmpty()
     password: string;
+
+    @IsNotEmpty()
     name: string;
+    
     address: string;
 }
