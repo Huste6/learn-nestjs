5da7494e8d2d2743881c955453ac16ae9f8ab100
Huste6 <quannghihai1@gmail.com>
2024-10-06 20:50:45 +0700

    learn-nest

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,6 @@
+//data transfer object
+export class CreateUserDto {
+    email: string;
+    password: string;
+    name: string;
+}
---
+++ src/users/users.controller.ts
@@ -0,0 +1,43 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+@Controller('users') // -> /users
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+  // controller la thang dieu huong, phan phoi cong viec 
+  //service co kha nang ket noi voi database
+  
+  @Post()
+  create(
+    @Body("email") myEmail: string,
+    @Body("password") password: string,
+    @Body("name") name: string,
+  )
+  {
+    //const myEmail = req.body.email;
+
+    return this.usersService.create(myEmail,password,name);
+  }
+
+  @Get()
+  findAll() {
+    return this.usersService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.usersService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return this.usersService.update(+id, updateUserDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.usersService.remove(+id);
+  }
+}
